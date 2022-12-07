bc091a11612e4c5965a87fdc0c0cd53e79fb54e7
adrian <adrian.lin.1.0.0@gmail.com>
2022-12-07 17:43:15 +0800

    docs : duplicated test name in euler001

--- 001.Multiples_of_3_and_5/euler001_test.go
+++ 001.Multiples_of_3_and_5/euler001_test.go
@@ -12,7 +12,7 @@ func TestMultiplesOf3And5(t *testing.T) {
 		want int64
 	}{
 		{name: "test1", args: args{n: 10}, want: 23},
-		{name: "test1", args: args{n: 100}, want: 2318},
+		{name: "test2", args: args{n: 100}, want: 2318},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
